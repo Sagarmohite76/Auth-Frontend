f1108a4c092390415e2df0de410e9417c9d873ed
Sagar Mohite <sagarmohite2325@gmail.com>
2026-04-08 18:05:48 +0530

    add...

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom'; // <-- import
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename="/Auth-Frontend/"> {/* <-- wrap App with basename */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
